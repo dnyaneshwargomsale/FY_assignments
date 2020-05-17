e3611fdd9391cb89514706071f0f68ed7a93b82c
dnyaneshwargomsale <64888026+dnyaneshwargomsale@users.noreply.github.com>
2020-05-17 10:46:39 +0530

    Update and rename ass1.py to ass1_question.py

---
+++ ass1_question.py
@@ -0,0 +1,35 @@
+
+'''Complete the algorithms and Flow chart for the following problem statements
+
+Q1. Write an Algorithm and Draw a flowchart to find the largest among three different numbers entered by the user. Note Number should be taken from the user.
+
+Q2. Write an algorithm and draw a flowchart for finding simple interest on bank account deposit.
+
+Q3. Write an Algorithm and draw a flowchart to find the sum of first 10 numbers.
+
+Q4. Write an Algorithm and draw a flowchart to find the sum of first 10 even number and odd number.
+
+Q5. Write an algorithm and draw a flowchart for finding the area of a circle.
+
+Q6. Write an algorithm and draw a flowchart to check whether a given letter is consonant or vowel.
+
+Q7. Write an algorithm and draw a flowchart to swap values of variables using the third variable and Print it as output.
+
+Q8. Write an algorithm and draw a flowchart to find a grade for respective marks entered by the user. Grade range as follows.
+
+AA: 70 -100
+
+BB: 60 -69
+
+CC: 50-59
+
+DD: 40-49
+
+FF: 0 -40
+
+Q9. Write an algorithm and draw a flowchart to find the first 10 odd and even number and also find how many final comparisons are made to find odd and even number.
+
+Q10. Write an algorithm and draw a flowchart to the factorial of number from 1 to 5.
+
+Create one pdf file only for all the solutions and upload it.'''
+
